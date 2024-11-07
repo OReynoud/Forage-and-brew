9813021eeaa3ffc2ed07f080a53da49cdb22ff81
Tintin <v.rousselle@rubika-edu.com>
2024-11-07 18:53:05 +0100

    feat: complete ingredient scriptable object

--- Forage and Brew/Assets/Scripts/Ingredients/IngredientEnumLibrary.cs
+++ Forage and Brew/Assets/Scripts/Ingredients/IngredientEnumLibrary.cs
@@ -1,4 +1,29 @@
+using System;
+
 public enum IngredientType
 {
     Mushroom
+}
+
+[Flags]
+public enum Biome
+{
+    Forest = 1 << 0
+}
+
+[Flags]
+public enum SpawnLocation
+{
+    FlatLand = 1 << 0,
+    BaseOfTree = 1 << 1,
+    TreeTrunk = 1 << 2,
+    DeadTreeTrunk = 1 << 3,
+    Brush = 1 << 4,
+    RiverBank = 1 << 5,
+    RiverBed = 1 << 6,
+    Pond = 1 << 7,
+    Mud = 1 << 8,
+    Rock = 1 << 9,
+    RockWall = 1 << 10,
+    Cliff = 1 << 11
 }
--- Forage and Brew/Assets/Scripts/Ingredients/IngredientValuesSo.cs
+++ Forage and Brew/Assets/Scripts/Ingredients/IngredientValuesSo.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "D_IngredientValues", menuName = "Ingredients/IngredientValuesSo")]
 public class IngredientValuesSo : ScriptableObject
 {
+    [field: SerializeField] public string Name { get; private set; }
+    [field: SerializeField] public string Description { get; private set; }
     [field: SerializeField] public IngredientType Type { get; private set; }
+    [field: SerializeField] public Biome Biomes { get; private set; }
+    [field: SerializeField] public SpawnLocation SpawnLocations { get; private set; }
+    [field: SerializeField] public WeatherStateSo[] WeatherStates { get; private set; }
+    [field: SerializeField] public LunarCycleStateSo[] LunarCycleStates { get; private set; }
     [field: SerializeField] public GameObject MeshGameObject { get; private set; }
 }
