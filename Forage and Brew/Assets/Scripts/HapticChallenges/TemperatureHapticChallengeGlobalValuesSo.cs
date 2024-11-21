3b6f5aaa3028c14fd9081a9459eaefcd69a9f227
Tintin <v.rousselle@rubika-edu.com>
2024-11-21 15:23:18 +0100

    feat: temperature challenge

--- Forage and Brew/Assets/Scripts/HapticChallenges/TemperatureHapticChallengeGlobalValuesSo.cs
+++ Forage and Brew/Assets/Scripts/HapticChallenges/TemperatureHapticChallengeGlobalValuesSo.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "D_TemperatureHapticChallengeGlobalValues", menuName = "Haptic Challenges/TemperatureHapticChallengeGlobalValuesSo")]
 public class TemperatureHapticChallengeGlobalValuesSo : ScriptableObject
 {
-    [field: SerializeField] [field: Range(0f, 1f)] public float LowHeatPart { get; private set; }
-    [field: SerializeField] [field: Range(0f, 1f)] public float HighHeatPart { get; private set; }
+    [field: SerializeField] [field: Range(0f, 1f)] public float LowHeatMinValue { get; private set; }
+    [field: SerializeField] [field: Range(0f, 1f)] public float LowHeatMaxValue { get; private set; }
+    [field: SerializeField] [field: Range(0f, 1f)] public float MediumHeatMinValue { get; private set; }
+    [field: SerializeField] [field: Range(0f, 1f)] public float MediumHeatMaxValue { get; private set; }
+    [field: SerializeField] [field: Range(0f, 1f)] public float HighHeatMinValue { get; private set; }
+    [field: SerializeField] [field: Range(0f, 1f)] public float HighHeatMaxValue { get; private set; }
     [field: SerializeField] [field: Min(0f)] public float HeatDecreaseSpeed { get; private set; }
     [field: SerializeField] [field: Min(0f)] public float HeatIncreaseDuration { get; private set; }
     [field: SerializeField] [field: Min(0f)] public float HeatIncreaseQuantity { get; private set; }
