0249e064391c44b57f1f2c76db48c06de0e2c779
REYNOUD <o.reynoud@rubika-edu.com>
2024-12-18 16:20:00 +0100

    refactor: codex

--- Forage and Brew/Assets/Scripts/GameDontDestroyOnLoadManager.cs
+++ Forage and Brew/Assets/Scripts/GameDontDestroyOnLoadManager.cs
@@ -21,8 +21,10 @@ public class GameDontDestroyOnLoadManager : MonoBehaviour
     public List<List<PotionValuesSo>> OrderPotions { get; private set; } = new();
     
     // Letters
-    [field: SerializeField] public bool GenerateLetters { get; set; }
-    [field: SerializeField] public List<LetterContentSo> AllLetters { get; set; } = new();
+    public bool HasChosenLettersToday { get; set; }
+    public List<LetterContentSo> AllLetters { get; set; } = new();
+    public List<LetterContentSo> MailBoxLetters { get; set; } = new();
+    
     
     
     private void Awake()
