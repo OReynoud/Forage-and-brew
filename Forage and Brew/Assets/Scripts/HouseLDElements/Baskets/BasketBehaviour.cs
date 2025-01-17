624c63bd74a231a1a470cbc83d1dd9f51ac528d7
Tintin <the.tintin.picard@hotmail.com>
2025-01-17 10:37:04 +0100

    feat: input change basket + vfx

--- Forage and Brew/Assets/Scripts/HouseLDElements/Baskets/BasketBehaviour.cs
+++ Forage and Brew/Assets/Scripts/HouseLDElements/Baskets/BasketBehaviour.cs
@@ -3,9 +3,36 @@ using UnityEngine;
 public class BasketBehaviour : MonoBehaviour
 {
     [SerializeField] protected Transform meshParentTransform;
+    [SerializeField] protected GameObject meshGameObject;
+    [SerializeField] protected Collider basketCollider;
+    [SerializeField] protected Collider basketTrigger;
     [SerializeField] protected GameObject interactInputCanvasGameObject;
     [SerializeField] protected GameObject cancelInputCanvasGameObject;
+    [field: SerializeField] public BasketVfxManager BasketVfxManager { get; private set; }
     public bool DoesNeedToCheckAvailability { get; set; }
+    
+    private bool _hasToBeDisabled;
+    private bool _hasToBeEnabled;
+    private float _currentTimeLeft;
+    
+    
+    private void Update()
+    {
+        if (_hasToBeDisabled || _hasToBeEnabled)
+        {
+            _currentTimeLeft -= Time.deltaTime;
+
+            if (_currentTimeLeft > 0f) return;
+            
+            meshParentTransform.gameObject.SetActive(_hasToBeEnabled);
+            meshGameObject.SetActive(_hasToBeEnabled);
+            basketCollider.enabled = _hasToBeEnabled;
+            basketTrigger.enabled = _hasToBeEnabled;
+            enabled = _hasToBeEnabled;
+            _hasToBeDisabled = false;
+            _hasToBeEnabled = false;
+        }
+    }
 
 
     public void EnableInteract()
@@ -27,5 +54,30 @@ public class BasketBehaviour : MonoBehaviour
     public void DisableCancel()
     {
         cancelInputCanvasGameObject.SetActive(false);
+    }
+    
+    
+    public void StartEnable(float duration)
+    {
+        _hasToBeEnabled = true;
+        StopDisable();
+        _currentTimeLeft = duration;
+    }
+    
+    public void StopEnable()
+    {
+        _hasToBeEnabled = false;
+    }
+    
+    public void StartDisable(float duration)
+    {
+        _hasToBeDisabled = true;
+        StopEnable();
+        _currentTimeLeft = duration;
+    }
+    
+    public void StopDisable()
+    {
+        _hasToBeDisabled = false;
     }
 }
