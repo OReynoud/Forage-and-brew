a90fd40748ef0074563597c99745933b3639b2fa
Oscar Reynoud <o.reynoud@rubika-edu.com>
2025-01-20 06:35:36 +0100

    All music

--- Forage and Brew/Assets/Scripts/Visuals/CharacterVfxManager.cs
+++ Forage and Brew/Assets/Scripts/Visuals/CharacterVfxManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -8,11 +9,17 @@ public class CharacterVfxManager : MonoBehaviour
 
     [Header("Rain")]
     [SerializeField] private WeatherStateSo rainWeatherState;
+    [SerializeField] private WeatherStateSo cloudyWeatherState;
+    [SerializeField] private WeatherStateSo sunnyWeatherState;
     [SerializeField] private GameObject rainVfxGameObject;
     [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource ambianceSource;
     
-    [SerializeField] private AudioResource musicHouse;
-    [SerializeField] private AudioResource musicRainForest;
+    [SerializeField] private AudioResource[] musicHouse;
+    [SerializeField] private AudioResource[] musicForest;
+    [SerializeField] private AudioResource[] ambianceForest;
+    [SerializeField] private AudioResource[] musicSwamp;
+    [SerializeField] private AudioResource[] musicOutdoor;
 
     
     private void Awake()
@@ -30,12 +37,58 @@ public class CharacterVfxManager : MonoBehaviour
 
     public void CheckForRainVfx(Scene scene)
     {
-        if (scene == Scene.Biome1 && WeatherManager.Instance.CurrentWeatherStates[Biome.Forest].WeatherStateSo == rainWeatherState)
+        switch (scene)
         {
-            musicSource.resource = musicRainForest;
-            musicSource.Play();
-            PlayRainVfx();
+            case Scene.House:
+                musicSource.resource = musicHouse[0];
+                musicSource.Play();
+                break;
+            case Scene.Outdoor:
+                musicSource.resource = musicOutdoor[0];
+                musicSource.Play();
+                break;
+            case Scene.Biome1:
+                if (WeatherManager.Instance.CurrentWeatherStates[Biome.Forest].WeatherStateSo == cloudyWeatherState)
+                {
+                    musicSource.resource = musicForest[0];
+                    musicSource.Play();
+                    ambianceSource.resource = ambianceForest[0];
+                    ambianceSource.Play();
+                }
+                else if (WeatherManager.Instance.CurrentWeatherStates[Biome.Forest].WeatherStateSo == sunnyWeatherState)
+                {
+                    musicSource.resource = musicForest[1];
+                    musicSource.Play();
+                }
+                else if (WeatherManager.Instance.CurrentWeatherStates[Biome.Forest].WeatherStateSo == rainWeatherState)
+                {
+                    musicSource.resource = musicForest[2];
+                    musicSource.Play();
+                    PlayRainVfx();
+                }
+                break;
+            case Scene.Biome2:
+                if (WeatherManager.Instance.CurrentWeatherStates[Biome.Swamp].WeatherStateSo == cloudyWeatherState)
+                {
+                    musicSource.resource = musicSwamp[0];
+                    musicSource.Play();
+                }
+                else if (WeatherManager.Instance.CurrentWeatherStates[Biome.Swamp].WeatherStateSo == sunnyWeatherState)
+                {
+                    musicSource.resource = musicSwamp[1];
+                    musicSource.Play();
+                }
+                else if (WeatherManager.Instance.CurrentWeatherStates[Biome.Swamp].WeatherStateSo == rainWeatherState)
+                {
+                    musicSource.resource = musicSwamp[2];
+                    musicSource.Play();
+                    PlayRainVfx();
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(scene), scene, null);
         }
+        
 
         if (scene == Scene.Biome2 && WeatherManager.Instance.CurrentWeatherStates[Biome.Swamp].WeatherStateSo == rainWeatherState)
         {
@@ -43,11 +96,7 @@ public class CharacterVfxManager : MonoBehaviour
         }
 
         
-        if (scene == Scene.House && GameDontDestroyOnLoadManager.Instance.CurrentTimeOfDay == TimeOfDay.Nighttime)
-        {
-            musicSource.resource = musicHouse;
-            musicSource.Play();
-        }
+
     }
     
     public void PlayRainVfx()
