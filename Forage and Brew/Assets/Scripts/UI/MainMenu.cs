618e123db9d80eadb32e91b6b5a88386ca925413
LANGER <e.langer@rubika-edu.com>
2025-06-20 16:25:26 +0200

    preset build

--- Forage and Brew/Assets/Scripts/UI/MainMenu.cs
+++ Forage and Brew/Assets/Scripts/UI/MainMenu.cs
@@ -25,7 +25,7 @@ public class MainMenu : MonoBehaviour
     
     public void StartGame()
     {
-        SceneManager.LoadScene("SC_HouseTintin_Perso");
+        SceneManager.LoadScene("SC_NewHouse");
     }
 
     public void ShowOptions()
