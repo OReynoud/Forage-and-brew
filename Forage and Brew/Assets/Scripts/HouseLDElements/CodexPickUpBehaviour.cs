dad2bb5ecd029c2506804f310638e12b68d67cdb
Tintin <the.tintin.picard@hotmail.com>
2025-06-27 12:50:39 +0200

    feat: sparkle particle system on codex pickup + other little fixes

--- Forage and Brew/Assets/Scripts/HouseLDElements/CodexPickUpBehaviour.cs
+++ Forage and Brew/Assets/Scripts/HouseLDElements/CodexPickUpBehaviour.cs
@@ -1,43 +1,28 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CodexPickUpBehaviour : MonoBehaviour, ICinematicInteraction
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Animator animator;
-    public Transform LocationToWalk;
-    public GameObject localCanvas;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform locationToWalk;
+    [SerializeField] private GameObject localCanvas;
+    [SerializeField] private List<ParticleSystem> sparkleEffects;
     
-    void Start()
+    private static readonly int IsOpen = Animator.StringToHash("IsOpen");
+
+    
+    private void Start()
     {
         if (GameDontDestroyOnLoadManager.Instance.codexIsUnlocked)
+        {
             gameObject.SetActive(false);
-        
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        CharacterInteractController.Instance.CurrentNearCinematicInteraction = this;
-        localCanvas.SetActive(true);
-        animator.SetBool("IsOpen",true);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        CharacterInteractController.Instance.CurrentNearCinematicInteraction = null;
-        localCanvas.SetActive(false);
-        animator.SetBool("IsOpen",false);
-    }
+    
 
     public void StartInteraction()
     {
-        CharacterMovementController.Instance.TriggerWalkTransition(LocationToWalk.position);
+        CharacterMovementController.Instance.TriggerWalkTransition(locationToWalk.position);
         CharacterMovementController.Instance.FinishWalkToLocation.AddListener(PickupCodex);
         CharacterInputManager.Instance.DisableInputs();
     }
@@ -47,12 +32,36 @@ public class CodexPickUpBehaviour : MonoBehaviour, ICinematicInteraction
         GameDontDestroyOnLoadManager.Instance.codexIsUnlocked = true;
         CharacterMovementController.Instance.FinishWalkToLocation.RemoveListener(PickupCodex);
         CharacterInputManager.Instance.EnableInputs();
-        CharacterMovementController.Instance.transform.rotation = LocationToWalk.rotation;
+        CharacterMovementController.Instance.transform.rotation = locationToWalk.rotation;
         CharacterInputManager.Instance.EnterCodexMethod();
         gameObject.SetActive(false);
         CharacterInteractController.Instance.CurrentNearCinematicInteraction = null;
         
         CharacterInputManager.Instance.EnableMoveInputs();
+    }
+    
 
+    private void OnTriggerEnter(Collider other)
+    {
+        CharacterInteractController.Instance.CurrentNearCinematicInteraction = this;
+        localCanvas.SetActive(true);
+        animator.SetBool(IsOpen, true);
+        
+        foreach (ParticleSystem sparkleEffect in sparkleEffects)
+        {
+            sparkleEffect.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CharacterInteractController.Instance.CurrentNearCinematicInteraction = null;
+        localCanvas.SetActive(false);
+        animator.SetBool(IsOpen, false);
+        
+        foreach (ParticleSystem sparkleEffect in sparkleEffects)
+        {
+            sparkleEffect.Stop();
+        }
     }
 }
