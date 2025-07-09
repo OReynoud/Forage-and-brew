528b3d805431f4229e0ee3cbfc0f13543ce8ec93
Tintin <the.tintin.picard@hotmail.com>
2025-07-09 14:13:44 +0200

    feat: new lerp cam in house scene

--- Forage and Brew/Assets/Scripts/Camera/CameraPreset.cs
+++ Forage and Brew/Assets/Scripts/Camera/CameraPreset.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CameraPresets", menuName = "Scriptable Objects/CameraPreset")]
 public class CameraPreset : ScriptableObject
 {
+    public Color groupColor = Color.white;
     public bool isFixedCameraPos;
     [ShowIf("isFixedCameraPos")] public bool isFixedCameraRotation = true;
     public Vector3 cameraOffset;
@@ -15,7 +16,7 @@ public class CameraPreset : ScriptableObject
     [Range(0,1)]public float rotationLerp = 0.02f;
     [Range(0,1)]public float focalLerp = 0.07f;
     
-    [BoxGroup("Adjustable Variables")] public Vector3 posMaxClamp = new Vector3(0,0,100);
-    [BoxGroup("Adjustable Variables")] public Vector3 posMinClamp = new Vector3(0,0,-100);
+    [BoxGroup("Adjustable Variables")] public Vector3 posMaxClamp = new(0,0,100);
+    [BoxGroup("Adjustable Variables")] public Vector3 posMinClamp = new(0,0,-100);
     [BoxGroup("Adjustable Variables")] public AnimationCurve transitionCurve = AnimationCurve.EaseInOut(0,0,1,1);
 }
