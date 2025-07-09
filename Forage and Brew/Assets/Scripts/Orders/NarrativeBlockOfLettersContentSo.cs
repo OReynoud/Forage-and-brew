66a738a201e6464d4888721c34bdf51a192f9284
Tintin <the.tintin.picard@hotmail.com>
2025-07-09 15:15:11 +0200

    feat: move part of narrative block behaviour for more flexibility

--- Forage and Brew/Assets/Scripts/Orders/NarrativeBlockOfLettersContentSo.cs
+++ Forage and Brew/Assets/Scripts/Orders/NarrativeBlockOfLettersContentSo.cs
@@ -6,9 +6,4 @@ public class NarrativeBlockOfLettersContentSo : ScriptableObject
     [field: SerializeField] public string Name { get; private set; }
     [field: SerializeField] public LetterContentSo[] Content { get; private set; }
     [field: SerializeField] public int RequiredQuestProgressionIndex { get; private set; }
-    [field: SerializeField] public bool CanAdvanceQuestProgressionIndex { get; set; }
-    
-    [field: SerializeField] public int TimeForLetterAfterSuccess { get; set; }
-    
-    [field: SerializeField] public int TimeForLetterAfterFailure { get; set; }
 }
