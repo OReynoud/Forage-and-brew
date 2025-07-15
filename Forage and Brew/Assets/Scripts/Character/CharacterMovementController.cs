fc0b64ce783e9d30d219f5d6450c8d1257e1ae5e
Tintin <the.tintin.picard@hotmail.com>
2025-07-15 17:10:45 +0200

    fix: add info on debug for error

--- Forage and Brew/Assets/Scripts/Character/CharacterMovementController.cs
+++ Forage and Brew/Assets/Scripts/Character/CharacterMovementController.cs
@@ -98,7 +98,7 @@ public class CharacterMovementController : MonoBehaviour
                 walkAudioSource.resource = walkSwamp;
                 break;
             default:
-                throw new ArgumentOutOfRangeException(nameof(scene), scene, null);
+                throw new ArgumentOutOfRangeException(nameof(scene), scene, scene.ToString());
         }
     }
     
