30812a5c62e13aaad456ae9c4521f2d4b02dded3
MSI Félix <felix.sun210302@gmail.com>
2025-08-04 16:54:46 +0200

    other: retake + balancing tree path & ile

--- Forage and Brew/Assets/Scripts/Ingredients/IngredientEnumLibrary.cs
+++ Forage and Brew/Assets/Scripts/Ingredients/IngredientEnumLibrary.cs
@@ -9,14 +9,16 @@ public enum SpawnLocation
 {
     None = 0,
     FlatLand = 1 << 0,
-    BaseOfTree = 1 << 1,
+    BaseOfTreeWithSmallBushes = 1 << 1,
     DeadTreeTrunk = 1 << 2,
     TreeStump = 1 << 3,
     Bush = 1 << 4,
-    RiverBank = 1 << 5,
+    MuddyBank = 1 << 5,
     SubmergedLand = 1 << 6,
     BaseOfGiantTree = 1 << 7,
     MagicTreeStump = 1 << 8,
     MagicDeadTreeTrunk = 1 << 9,
-    Rock = 1 << 10
+    Rock = 1 << 10,
+    GrassyPatch = 1 << 11,
+    ExposedRoots = 1 << 12,
 }
