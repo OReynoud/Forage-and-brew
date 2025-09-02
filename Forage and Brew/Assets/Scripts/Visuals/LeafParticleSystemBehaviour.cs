3f5c60047e8a949b7c7391651d1e3aa8857d7f23
BlookyClem <c.lai@rubika-edu.com>
2025-09-02 23:07:26 +0200

    VFX explications de Tintin

---
+++ Forage and Brew/Assets/Scripts/Visuals/LeafParticleSystemBehaviour.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class LeafParticleSystemBehaviour : MonoBehaviour
+{
+    private void OnParticleTrigger()
+    {
+        throw new NotImplementedException();
+    }
+}
