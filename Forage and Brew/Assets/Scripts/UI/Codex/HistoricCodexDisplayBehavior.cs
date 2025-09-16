dd179633a0aa57e67b8c750a9e2a88b29084d85d
OReynoud <95909515+OReynoud@users.noreply.github.com>
2025-09-16 18:41:24 +0200

    wip: historic ratio slider

--- Forage and Brew/Assets/Scripts/UI/Codex/HistoricCodexDisplayBehavior.cs
+++ Forage and Brew/Assets/Scripts/UI/Codex/HistoricCodexDisplayBehavior.cs
@@ -1,7 +1,10 @@
 
+using System;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class HistoricCodexDisplayBehavior : PageBehavior
 {
@@ -13,6 +16,7 @@ public class HistoricCodexDisplayBehavior : PageBehavior
     public TextMeshProUGUI description2Text;
     public GameObject tampon;
     public LetterContentSo OriginLetter;
+    [Range(0, 1)] public float historicPagesRatio = 0.5f; 
 
     
     public override void InitHistoric(LetterContentSo originLetter, LetterContentSo successLetter)
@@ -35,5 +39,29 @@ public class HistoricCodexDisplayBehavior : PageBehavior
         backGround1.transform.rotation = Quaternion.identity;
         tampon.SetActive(false);
         OriginLetter = originLetter;
+    }
+
+    private void OnValidate()
+    {
+        if (!Application.isPlaying)
+        {
+            var oui = backGround1.GetComponent<RectTransform>();
+            oui.offsetMin = new Vector2(oui.offsetMin.x,
+            Mathf.Lerp(0,1000,historicPagesRatio));
+            //oui.sizeDelta = new Vector2(oui.sizeDelta.x, )
+            // oui.rect.center = Vector2.zero;
+            //     oui.rect.yMax);
+            // oui.;
+            // Debug.Log(oui.rect.yMin);
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        var oui = backGround1.GetComponent<RectTransform>();
+        Handles.Label(oui.anchoredPosition, "pos");
+        Debug.Log(oui.offsetMin.y);
+        Debug.Log(oui.sizeDelta);
+        Handles.Label(oui.rect.max, "Max");
     }
 }
