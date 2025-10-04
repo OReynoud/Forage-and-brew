661bba871e3bc37d3dafcdde15267d8ba0fc871d
OReynoud <95909515+OReynoud@users.noreply.github.com>
2025-10-04 18:47:48 +0200

    wip: fix historic page

--- Forage and Brew/Assets/Scripts/UI/Codex/HistoricCodexDisplayBehavior.cs
+++ Forage and Brew/Assets/Scripts/UI/Codex/HistoricCodexDisplayBehavior.cs
@@ -1,4 +1,3 @@
-
 using System;
 using TMPro;
 using UnityEditor;
@@ -8,7 +7,6 @@ using Image = UnityEngine.UI.Image;
 
 public class HistoricCodexDisplayBehavior : PageBehavior
 {
-    
     public TextMeshProUGUI clientNameText;
     public Image backGround1;
     public Image backGround2;
@@ -16,13 +14,13 @@ public class HistoricCodexDisplayBehavior : PageBehavior
     public TextMeshProUGUI description2Text;
     public GameObject tampon;
     public LetterContentSo OriginLetter;
-    
-    
+
+
     public float backGroundSpacing;
     public float backGroundRim;
-    [Range(0, 1)] public float historicPagesRatio = 0.5f; 
+    [Range(0, 1)] public float historicPagesRatio = 0.5f;
 
-    
+
     public override void InitHistoric(LetterContentSo originLetter, LetterContentSo successLetter)
     {
         clientNameText.text = originLetter.Client.Name;
@@ -30,44 +28,51 @@ public class HistoricCodexDisplayBehavior : PageBehavior
         description2Text.text = successLetter ? successLetter.TextContent : "";
         backGround1.color = backGround2.color = originLetter.Client.AssociatedColor;
         OriginLetter = originLetter;
-    }    
+    }
+
     public void InitHistoric(LetterContentSo originLetter)
     {
         clientNameText.text = originLetter.Client.Name;
         description1Text.text = originLetter.TextContent;
         backGround2.gameObject.SetActive(false);
         backGround1.color = originLetter.Client.AssociatedColor;
-        backGround1.rectTransform.offsetMax = new Vector2(-53, -30);
-        backGround1.rectTransform.offsetMin = new Vector2(53, 47);
+        //backGround1.rectTransform.offsetMax = new Vector2(-53, -30);
+        //backGround1.rectTransform.offsetMin = new Vector2(53, 47);
         description2Text.enabled = false;
         backGround1.transform.rotation = Quaternion.identity;
         tampon.SetActive(false);
         OriginLetter = originLetter;
+        var backGround1Rect = backGround1.GetComponent<RectTransform>();
+        backGround1Rect.offsetMin = new Vector2(backGround1Rect.offsetMin.x,
+            Mathf.Lerp(0, 1000, historicPagesRatio) - backGroundSpacing);
+        backGround1Rect.offsetMax = new Vector2(backGround1Rect.offsetMax.x,
+            -backGroundRim);
+
+        var backGround2Rect = backGround2.GetComponent<RectTransform>();
+        backGround2Rect.offsetMax = new Vector2(backGround2Rect.offsetMax.x,
+            Mathf.Lerp(-1000, 0, historicPagesRatio) + backGroundSpacing);
+        backGround2Rect.offsetMin = new Vector2(backGround2Rect.offsetMin.x,
+            backGroundRim);
     }
 
     private void OnValidate()
     {
-        if (!Application.isPlaying)
-        {
-            var backGround1Rect = backGround1.GetComponent<RectTransform>();
-            backGround1Rect.offsetMin = new Vector2(backGround1Rect.offsetMin.x,
-            Mathf.Lerp(0,1000,historicPagesRatio) - backGroundSpacing);
-            backGround1Rect.offsetMax = new Vector2(backGround1Rect.offsetMax.x,
-                -backGroundRim);
-            
-            var backGround2Rect = backGround2.GetComponent<RectTransform>();
-            backGround2Rect.offsetMax = new Vector2(backGround2Rect.offsetMax.x,
-                Mathf.Lerp(-1000,0,historicPagesRatio) + backGroundSpacing);
-            backGround2Rect.offsetMin = new Vector2(backGround2Rect.offsetMin.x,
-                backGroundRim);
-            
-            //oui.sizeDelta = new Vector2(oui.sizeDelta.x, )
-            // oui.rect.center = Vector2.zero;
-            //     oui.rect.yMax);
-            // oui.;
-            // Debug.Log(oui.rect.yMin);
-        }
+        var backGround1Rect = backGround1.GetComponent<RectTransform>();
+        backGround1Rect.offsetMin = new Vector2(backGround1Rect.offsetMin.x,
+            Mathf.Lerp(0, 1000, historicPagesRatio) - backGroundSpacing);
+        backGround1Rect.offsetMax = new Vector2(backGround1Rect.offsetMax.x,
+            -backGroundRim);
+
+        var backGround2Rect = backGround2.GetComponent<RectTransform>();
+        backGround2Rect.offsetMax = new Vector2(backGround2Rect.offsetMax.x,
+            Mathf.Lerp(-1000, 0, historicPagesRatio) + backGroundSpacing);
+        backGround2Rect.offsetMin = new Vector2(backGround2Rect.offsetMin.x,
+            backGroundRim);
+
+        //oui.sizeDelta = new Vector2(oui.sizeDelta.x, )
+        // oui.rect.center = Vector2.zero;
+        //     oui.rect.yMax);
+        // oui.;
+        // Debug.Log(oui.rect.yMin);
     }
-
-
 }
