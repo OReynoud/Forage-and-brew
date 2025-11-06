6cfdf7087cdfe7c040716ecdeec065d30d89d1cd
OReynoud <95909515+OReynoud@users.noreply.github.com>
2025-11-06 17:49:35 +0100

    integration: bird flyoff behavior

--- Forage and Brew/Assets/IdleBirdBehavior.cs
+++ Forage and Brew/Assets/IdleBirdBehavior.cs
@@ -1,8 +1,12 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class IdleBirdBehavior : MonoBehaviour
 {
     private static readonly int CycleOffset = Animator.StringToHash("CycleOffset");
+    private static readonly int DoFly = Animator.StringToHash("DoFly");
+    private Collider col;
 
     private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -10,11 +14,21 @@ public class IdleBirdBehavior : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         animator.SetFloat(CycleOffset, Random.value);
+        col = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            animator.SetTrigger(DoFly);
+            col.enabled = false;
+        }
     }
 }
